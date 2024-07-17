9bd4a7fcd9cfb15fd700d7435e2152fdc622ebb9
ksmooi <kaishianmooi@gmail.com>
2024-07-17 16:37:39 +0800

    schema of pgsql database

---
+++ db_schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE products (
+    id BIGINT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    quantity INT NOT NULL
+);
+
+insert into products (id, name, quantity) values (105442, 'milk', 131);
+insert into products (id, name, quantity) values (105443, 'bacon', 122);
+insert into products (id, name, quantity) values (105444, 'tuna', 34);
+insert into products (id, name, quantity) values (105445, 'eggs', 23);
+insert into products (id, name, quantity) values (105446, 'kombucha', 18);
+insert into products (id, name, quantity) values (105447, 'apples', 17);
+insert into products (id, name, quantity) values (105448, 'ice tea', 41);
+insert into products (id, name, quantity) values (105449, 'yogurt', 9);
